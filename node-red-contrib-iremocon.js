ac182f74923d181138369a6992db7db01bb65e4c
morikat <morika-t@outlook.com>
2015-08-11 18:22:55 +0900

    Fix node name

--- node-red-contrib-iremocon.js
+++ node-red-contrib-iremocon.js
@@ -1,14 +1,11 @@
 module.exports = function(RED) {
-    function IremoNode(config) {
+    function IRemoconNode(config) {
         RED.nodes.createNode(this,config);
         this.host=config.host;
         var node = this;
         console.log(this.host);
         this.on('input', function(msg) {
             var iRemocon = new require('iremocon');
-            //var iremo = new iRemocon('192.168.1.90');
-            //var ipadd= msg;
-            //console.log(ipadd);
             var iremo = new iRemocon(node.host);
             iremo.send('se', function(err, msg) {
                if (err) {
@@ -17,13 +14,11 @@ module.exports = function(RED) {
                  return;
                }
                //console.log(msg);
+               // e.g. ic;ok
                var msg2 = { payload: msg };
                node.send(msg2);
-               //msg.payload = msg2;
-               //node.send(msg);
-               // e.g. ic;ok
             }); 
         });
     }
-    RED.nodes.registerType("iremo",IremoNode);
+    RED.nodes.registerType("iremocon",IRemoconNode);
 }
